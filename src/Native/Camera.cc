c642974cc9b2e9d51b81d7864a62ca86ad0fb390
Craabby <69945662+Craabby@users.noreply.github.com>
2022-05-05 19:44:56 -0400

    worry about error later

--- src/Native/Camera.cc
+++ src/Native/Camera.cc
@@ -2,7 +2,7 @@
 #include <Native/Camera.h>
 #include <Native/FieldGroups.h>
 
-CameraEntity::CameraEntity(entityId arena)
-    : camera(new CameraGroup(arena))
-{
-}
+// CameraEntity::CameraEntity(entityId arena)
+//     : camera(new CameraGroup(arena))
+// {
+// }
